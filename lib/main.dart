d7dcb9b65ef0e7313ac16a03c5a57739b6c4843e
Albert Nguyen <alcoholicdad@Alberts-MacBook-Air.local>
2023-03-04 17:07:09 -0600

    Added NetworkImage to body

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Center(child: Text("Hello World")),
-  ));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.green,
+        appBar: AppBar(
+          title: const Text("I Am Rich"),
+          backgroundColor: Colors.green[900],
+        ),
+        body: const Image(
+          image: NetworkImage(
+              "https://drive.google.com/uc?export=view&id=1wIk6JJINM7p0PqtMuy9wvkRQtDg_uukm"),
+        ),
+      ),
+    ),
+  );
 }
